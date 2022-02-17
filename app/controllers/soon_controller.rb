c1994392d9fd091b6f9cefc85fbf903920a5325e
Gabriel Walsh <brotherclone@gmail.com>
2022-02-17 10:38:11 -0500

    set up

--- app/controllers/soon_controller.rb
+++ app/controllers/soon_controller.rb
@@ -1,5 +1,9 @@
 class SoonController < ApplicationController
-  def index
 
+
+
+  def index
+    @show_navigation = false
+    @show_breadcrumbs = false
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+require('packs/navigation/navigation')
 import "@fortawesome/fontawesome-free/css/all"
 import "channels"
 
