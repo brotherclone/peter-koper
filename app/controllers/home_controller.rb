da56e6f2b7f72e8d08fffc636e0d96c4a8501c2f
Gabriel Walsh <brotherclone@gmail.com>
2022-02-20 16:42:50 -0500

    set up but a mess

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    # @show_navigation = true
-    # @show_breadcrumbs = true
+    @show_navigation = true
+    @show_breadcrumbs = true
   end
+
+
 end
