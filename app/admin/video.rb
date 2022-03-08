fd191f5538b1f2751c58b08d3a2cbf4396886183
Gabriel Walsh <brotherclone@gmail.com>
2022-03-08 15:32:10 -0500

    rework model

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,13 +1,12 @@
 ActiveAdmin.register Video do
 
-  permit_params :is_live, :title, :url, :video_service, :video_id, :memory_id
+  permit_params :is_live, :memory_id, :file, :file_cache
 
   form(:html => { :multipart => true }) do |f|
     f.inputs do
-      f.input :title
-      f.input :url
-      f.input :video_service
-      f.input :video_id
+      f.input :file, :as => :file
+      f.input :file_cache, :as => :hidden
+      f.input :is_live
       f.input :memory_id, :label => 'Memory', :as => :select, :collection => Memory.all.map{|a| ["#{a.title}", a.id]}
     end
     f.actions
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,16 +6,4 @@ RSpec.describe Video, type: :model do
     it{ expect(build(:video)).to be_valid }
   end
 
-  describe 'Must have a video id' do
-    it { should validate_presence_of(:video_id) }
-  end
-
-  describe 'Must have a url' do
-    it { should validate_presence_of(:url) }
-  end
-
-  describe 'Must have a video service' do
-    it { should validate_presence_of(:video_service) }
-  end
-
 end
