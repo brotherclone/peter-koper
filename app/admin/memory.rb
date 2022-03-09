045d21b21cd3d16e65d63805306eb0ca7002930d
Gabriel Walsh <brotherclone@gmail.com>
2022-03-09 14:58:22 -0500

    this closes #45

--- app/admin/memory.rb
+++ app/admin/memory.rb
@@ -21,7 +21,8 @@ ActiveAdmin.register Memory do
     attributes_table do
       row :title
       row :body
-      row :category
+      row :categories
+      row :tags
       row :is_live
       row :image do |i|
         image_tag i.image.to_s, :style => "max-width:200px; height:auto"
