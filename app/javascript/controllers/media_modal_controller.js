4006c74cc6622f97dfc82e7891802991ef0cfa5b
Gabriel Walsh <brotherclone@gmail.com>
2022-03-15 13:21:28 -0400

    document viewer working

--- app/javascript/controllers/media_modal_controller.js
+++ app/javascript/controllers/media_modal_controller.js
@@ -1,30 +1,66 @@
 import { Controller } from "@hotwired/stimulus"
-import htmlpdfviewer from "html-pdf-viewer";
 import cloudinary from 'cloudinary-core'
 import MicroModal from 'micromodal'
 
 export default class extends Controller {
 
-
+    static targets = ["videoPlayerId", "documentReplaceId", "videoFileId", "apis", "pdf"]
 
     connect() {
         MicroModal.init();
-        console.log("connect");
-        // const html = document.getElementById('html');
-        // htmlpdfviewer(html, { output: { mode: 'display', container: '#iframeId', height: 800 } });
-    }
-
-    set(){
-        console.log("set")
     }
 
     showplyr(){
-        // console.log("video id", this.videoPlayerInfoTarget.dataset.videoId)
-        // console.log("video file id", this.videoPlayerInfoTarget.dataset.videoFileId)
-        // console.log("modal id", this.videoPlayerInfoTarget.dataset.modalId)
-        // let videoPlayerId = "video_player_"+this.videoPlayerInfoTarget.dataset.videoId;
-        // let cld = cloudinary.Cloudinary.new({ cloud_name: 'hodsx14x9' })
-        // document.player = cld.videoPlayer(videoPlayerId)
-        // document.player.source(this.videoPlayerInfoTarget.dataset.videoFileId).play();
+        let videoPlayerId = this.videoPlayerIdTarget.dataset.videplayerid;
+        let videoFile = this.videoFileIdTarget.dataset["videofileid"];
+        let cloudName = this.apisTarget.dataset.cloud;
+        let cld = cloudinary.Cloudinary.new({ cloud_name: cloudName })
+        document.player = cld.videoPlayer(videoPlayerId)
+        document.player.source(videoFile).play();
     }
+
+    hideplyr(){
+        document.player.stop();
+    }
+
+    showrdr(){
+        const viewerOptions = {
+            embedMode: "IN_LINE",
+            defaultViewMode: "FIT_PAGE",
+            showDownloadPDF: false,
+            showPrintPDF: false,
+            showLeftHandPanel: true,
+            showAnnotationTools: true
+        };
+        let clientID =  this.apisTarget.dataset.clientid;
+        let pdfUrl = this.pdfTarget.dataset.pdfurl;
+        let pdfFile = this.pdfTarget.dataset.pdffile;
+        let pdfView = this.pdfTarget.dataset.pdfview;
+        let adobeDCView = new AdobeDC.View({
+            clientId: clientID,
+            divId: pdfView,
+        });
+        console.log(pdfUrl,pdfFile);
+        adobeDCView.previewFile(
+            {
+                content: { promise: this.load() },
+                metaData: { fileName: pdfFile}
+            },viewerOptions);
+    }
+
+    load(){
+        return new Promise((resolve) => {
+            fetch( this.pdfTarget.dataset.pdfurl)
+                .then((resolve) => resolve.blob())
+                .then((blob)=>{
+                    resolve(blob.arrayBuffer())
+                })
+        })
+
+    }
+
+    hiderdr(){
+        console.log("hiderdr")
+    }
+
 }
